f2c4eba7eaa8a6890c57c34578d732473a3f68c5
Jesus Lechuga <95671796+Jesusleg95@users.noreply.github.com>
2025-07-01 16:22:38 -0600

    Add files via upload

--- crm-vue/src/components/Cliente.vue
+++ crm-vue/src/components/Cliente.vue
@@ -37,16 +37,16 @@
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
             <RouterLink
-                to="/"
+                :to="{ name: 'editar-cliente', params: { id: cliente.id }}"
                 class="text-indigo-600 hover:text-indigo-900 mr-5"
             >
                 Editar
             </RouterLink>
-            <Button
+            <button
                 class="text-red-600 hover:text-red-900"
             >
                 Eliminar
-            </Button>
+            </button>
         </td>
     </tr>
 </template>
---
+++ crm-vue/src/views/EditarClienteView.vue
@@ -0,0 +1,120 @@
+<script setup>
+    import { onMounted, reactive } from 'vue';
+    import { FormKit, FormKitSchema } from '@formkit/vue';
+    import { useRouter, useRoute } from 'vue-router';
+    import ClienteService from '../services/ClienteService';
+    import RouterLink from '../components/UI/RouterLink.vue';
+    import Heading from '../components/UI/Heading.vue';
+
+    const router = useRouter()
+    const route = useRoute()
+
+    const { id } = route.params
+
+    const formData = reactive({})
+
+    onMounted(() => {
+        ClienteService.obtenerCliente(id)
+            .then(({data}) => {
+                Object.assign(formData, data)
+            })
+            .catch(error => console.log(error))
+    })
+
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
+
+    const handleSubmit = (data) => {
+       
+    }
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="listado-clientes">
+                Volver
+            </RouterLink>
+        </div>
+
+        <Heading>{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
+                <FormKit
+                    type="form"
+                    submit-label="Agregar Cliente"
+                    incomplete-message="No se pudo enviar, revisa los mensajes"
+                    @submit="handleSubmit"
+                    :value="formData"
+                >
+                    <FormKit 
+                        type="text"
+                        label="Nombre"
+                        name="nombre"
+                        placeholder="Nombre del Cliente"
+                        validation="required"
+                        :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio'}"
+                        v-model="formData.nombre"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Apellido"
+                        name="apellido"
+                        placeholder="Apellido del Cliente"
+                        validation="required"
+                        :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio'}"
+                        v-model="formData.apellido"
+                    />
+
+                    <FormKit 
+                        type="email"
+                        label="Email"
+                        name="email"
+                        placeholder="Email del Cliente"
+                        validation="required|email"
+                        :validation-messages="{ required: 'El Email del Cliente es Obligatorio', email: 'Coloca un email válido'}"
+                        v-model="formData.email"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Teléfono"
+                        name="telefono"
+                        placeholder="Teléfono: XXX-XXX-XXXX"
+                        validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{ matches: 'El Formato no es válido' }"
+                        v-model="formData.telefono"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Empresa"
+                        name="empresa"
+                        placeholder="Empresa del Cliente"
+                        v-model="formData.empresa"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Puesto"
+                        name="puesto"
+                        placeholder="Puesto del Cliente"
+                        v-model="formData.puesto"
+                    />
+
+                </FormKit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+</style>
