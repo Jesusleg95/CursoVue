b5c4d5ea2c8fe5f7e536b36536716c9d6387725d
Jesus Lechuga <95671796+Jesusleg95@users.noreply.github.com>
2025-07-11 16:23:52 -0600

    Add files via upload

--- bebidas-vue/src/stores/bebidas.js
+++ bebidas-vue/src/stores/bebidas.js
@@ -1,8 +1,10 @@
 import { ref, reactive, onMounted } from "vue";
 import { defineStore } from "pinia";
 import APIService from "../services/APIService";
+import { useModalStore } from "./modal";
 
 export const useBebidasStore = defineStore('bebidas', () => {
+    const modal = useModalStore()
     const categorias = ref([])
     const busqueda = reactive({
         nombre: '',
@@ -10,21 +12,31 @@ export const useBebidasStore = defineStore('bebidas', () => {
     })
 
     const recetas = ref([])
+    const receta = ref({})
 
     onMounted(async function() {
         const {data: {drinks}} = await APIService.obtenerCategorias()
-        recetas.value = drinks
+        categorias.value = drinks
     })
 
     async function obtenerRecetas() {
         const {data: {drinks}} = await APIService.buscarRecetas(busqueda)
-        console.log(drinks)
+        recetas.value = drinks
+    }
+
+    async function seleccionarBebida(id) {
+        const {data: {drinks}} = await APIService.buscarReceta(id)
+        receta.value = drinks[0]
+
+        modal.handleClickModal()
     }
 
     return {
         categorias,
         busqueda,
+        recetas,
+        receta,
         obtenerRecetas,
-        recetas
+        seleccionarBebida
     }
 })
--- bebidas-vue/src/views/InicioView.vue
+++ bebidas-vue/src/views/InicioView.vue
@@ -8,7 +8,7 @@
 <template>
     <h1 class="text-6xl font-extrabold">Recetas</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 gap-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
         <Receta
             v-for="receta in bebidas.recetas"
             :receta="receta"
